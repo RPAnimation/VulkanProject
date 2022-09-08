bd0bc6f5887ccf33c2261e302ee65ffe0b2448fd
Radoslaw Pluta <rpanimation@icloud.com>
2022-09-08 18:27:26 +0200

    Added rasterizer

--- src/HelloTriangleApplication.cpp
+++ src/HelloTriangleApplication.cpp
@@ -338,6 +338,18 @@ void HelloTriangleApplication::createGraphicsPipeline()
 	viewportState.pViewports    = &viewport;
 	viewportState.scissorCount  = 1;
 	viewportState.pScissors     = &scissor;
+
+	VkPipelineRasterizationStateCreateInfo rasterizer{};
+	rasterizer.sType                = VK_STRUCTURE_TYPE_PIPELINE_RASTERIZATION_STATE_CREATE_INFO;
+	rasterizer.depthClampEnable     = VK_FALSE;
+	rasterizer.polygonMode          = VK_POLYGON_MODE_FILL;
+	rasterizer.lineWidth            = 1.0f;
+	rasterizer.cullMode             = VK_CULL_MODE_BACK_BIT;
+	rasterizer.frontFace            = VK_FRONT_FACE_CLOCKWISE;
+	rasterizer.depthBiasEnable      = VK_FALSE;
+	rasterizer.depthBiasClamp       = 0.0f;
+	rasterizer.depthBiasSlopeFactor = 0.0f;
+	rasterizer.depthBiasSlopeFactor = 0.0f;
 }
 
 void HelloTriangleApplication::mainLoop()
