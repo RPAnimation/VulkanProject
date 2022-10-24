c4d971969f57b3acb8f56edc2fe1afbb6c48a4ec
Radoslaw Pluta <rpanimation@icloud.com>
2022-10-24 18:11:40 +0200

    Added comments to specific parts of the class

--- src/HelloTriangleApplication.hpp
+++ src/HelloTriangleApplication.hpp
@@ -17,87 +17,98 @@ public:
     const char* ENGINE_NAME = "No engine";
     const uint32_t WIDTH = 800;
     const uint32_t HEIGHT = 600;
-    const std::vector<const char*> validationLayers = {"VK_LAYER_KHRONOS_validation"};
-    const int MAX_FRAMES_IN_FLIGHT = 2;
-    void run();
-    bool framebufferResized = false;
+	const std::vector<const char *> validationLayers     = {"VK_LAYER_KHRONOS_validation"};
+	const int                       MAX_FRAMES_IN_FLIGHT = 2;
+	void                            run();
+	bool                            framebufferResized = false;
 
-private:
-    GLFWwindow* window;
-    VkInstance instance;
-    VkDebugUtilsMessengerEXT debugMessanger;
-    VkPhysicalDevice physicalDevice = VK_NULL_HANDLE;
-    VkDevice logicalDevice = VK_NULL_HANDLE;
-    VkQueue graphicsQueue;
-    VkSurfaceKHR surface;
-    VkQueue presentQueue;
+  private:
+	// Device setup
+	GLFWwindow              *window;
+	VkInstance               instance;
+	VkDebugUtilsMessengerEXT debugMessanger;
+	VkPhysicalDevice         physicalDevice = VK_NULL_HANDLE;
+	VkDevice                 logicalDevice  = VK_NULL_HANDLE;
+	VkQueue                  graphicsQueue;
+	VkSurfaceKHR             surface;
+	VkQueue                  presentQueue;
 
-    VkSwapchainKHR swapChain;
-    std::vector<VkImage> swapChainImages;
-    VkFormat swapChainImageFormat;
-    VkExtent2D swapChainExtent;
-    std::vector<VkImageView> swapChainImageViews;
-    VkRenderPass renderPass;
-    VkDescriptorSetLayout descriptorSetLayout;
-    VkPipelineLayout pipelineLayout;
-    VkPipeline graphicsPipeline;
-    std::vector<VkFramebuffer> swapChainFramebuffers;
-    VkCommandPool commandPool;
-    VkBuffer vertexBuffer;
-    VkDeviceMemory vertexBufferMemory;
-    VkBuffer indexBuffer;
-    VkDeviceMemory indexBufferMemory;
-    std::vector<VkBuffer> uniformBuffers;
-    std::vector<VkDeviceMemory> uniformBuffersMemory;
-    VkDescriptorPool descriptorPool;
-    std::vector<VkDescriptorSet> descriptorSets;
-    std::vector<VkCommandBuffer> commandBuffers;
-    std::vector<VkFence> inFlightFences;
-    std::vector<VkSemaphore> imageAvailableSemaphores;
-    std::vector<VkSemaphore> renderFinishedSemaphores;
+	// Swap chain
+	VkSwapchainKHR             swapChain;
+	std::vector<VkImage>       swapChainImages;
+	VkFormat                   swapChainImageFormat;
+	VkExtent2D                 swapChainExtent;
+	std::vector<VkImageView>   swapChainImageViews;
+	VkRenderPass               renderPass;
+	VkDescriptorSetLayout      descriptorSetLayout;
+	VkPipelineLayout           pipelineLayout;
+	VkPipeline                 graphicsPipeline;
+	std::vector<VkFramebuffer> swapChainFramebuffers;
+	VkCommandPool              commandPool;
 
-    // Main phase
-    void initWindow();
-    void initVulkan();
-    void mainLoop();
-    void drawFrame();
-    void cleanup();
+	// Vertex/Index buffers -> UBO
+	VkBuffer                     vertexBuffer;
+	VkDeviceMemory               vertexBufferMemory;
+	VkBuffer                     indexBuffer;
+	VkDeviceMemory               indexBufferMemory;
+	std::vector<VkBuffer>        uniformBuffers;
+	std::vector<VkDeviceMemory>  uniformBuffersMemory;
+	VkDescriptorPool             descriptorPool;
+	std::vector<VkDescriptorSet> descriptorSets;
+	std::vector<VkCommandBuffer> commandBuffers;
 
-    // Vulkan phase
-    void createInstance();
-    void setupDebugMessanger();
-    void createSurface();
-    void pickPhysicalDevice();
-    void createLogicalDevice();
-    void recreateSwapChain();
-    void cleanupSwapChain();
-    void createSwapChain();
-    void createImageViews();
-    void createRenderPass();
-    void createDescriptorSetLayout();
-    void createGraphicsPipeline();
-    void createFramebuffers();
-    void createCommandPool();
-    void createVertexBuffer();
-    void createIndexBuffer();
-    void createUniformBuffers();
-    void createDescriptorPool();
-    void createDescriptorSets();
-    void createCommandBuffers();
-    void createSyncObjects();
+	// Sync objects
+	std::vector<VkFence>     inFlightFences;
+	std::vector<VkSemaphore> imageAvailableSemaphores;
+	std::vector<VkSemaphore> renderFinishedSemaphores;
 
-    // Helpful variables
-    uint32_t currentFrame = 0;
+	// Textures
+	VkImage        textureImage;
+	VkDeviceMemory textureImageMemory;
 
-    // Support functions
-    bool checkValidationLayerSupport();
-    static VKAPI_ATTR VkBool32 VKAPI_CALL
-    debugCallback(VkDebugUtilsMessageSeverityFlagBitsEXT messageSeverity,
-                  VkDebugUtilsMessageTypeFlagsEXT messageType,
-                  const VkDebugUtilsMessengerCallbackDataEXT* pCallbackData,
-                  void* pUserData);
-    void recordCommandBuffer(VkCommandBuffer buffer, uint32_t imageIndex);
-    void updateUniformBuffer(uint32_t currentImage);
+	// Main phase
+	void initWindow();
+	void initVulkan();
+	void mainLoop();
+	void drawFrame();
+	void cleanup();
+
+	// Vulkan phase
+	void createInstance();
+	void setupDebugMessanger();
+	void createSurface();
+	void pickPhysicalDevice();
+	void createLogicalDevice();
+	void recreateSwapChain();
+	void cleanupSwapChain();
+	void createSwapChain();
+	void createImageViews();
+	void createRenderPass();
+	void createDescriptorSetLayout();
+	void createGraphicsPipeline();
+	void createFramebuffers();
+	void createCommandPool();
+	void createTextureImage();
+	void createVertexBuffer();
+	void createIndexBuffer();
+	void createUniformBuffers();
+	void createDescriptorPool();
+	void createDescriptorSets();
+	void createCommandBuffers();
+	void createSyncObjects();
+
+	// Helpful variables
+	uint32_t currentFrame = 0;
+
+	// Support functions
+	bool checkValidationLayerSupport();
+	static VKAPI_ATTR VkBool32 VKAPI_CALL
+	     debugCallback(VkDebugUtilsMessageSeverityFlagBitsEXT      messageSeverity,
+	                   VkDebugUtilsMessageTypeFlagsEXT             messageType,
+	                   const VkDebugUtilsMessengerCallbackDataEXT *pCallbackData,
+	                   void	                                   *pUserData);
+	void recordCommandBuffer(VkCommandBuffer buffer, uint32_t imageIndex);
+	void updateUniformBuffer(uint32_t currentImage);
 };
 
 static void framebufferResizeCallback(GLFWwindow* window, int width, int height);
