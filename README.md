ef1d57392ff2ba8e59e70ae344b07dfd8c74853f
Radoslaw Pluta <rpanimation@icloud.com>
2022-11-25 19:42:38 +0100

    Added .obj file from external source

--- README.md
+++ README.md
@@ -10,3 +10,4 @@ Simple vulkan app that renders two projected rotating rectangles with texture ap
 ## External libraries
 - [STB (stb_image.h)](https://github.com/nothings/stb/blob/master/stb_image.h)
 - [Tiny obj loader (tiny_obj_loader.h)](https://github.com/tinyobjloader/tinyobjloader/blob/release/tiny_obj_loader.h)
+- [Efertiti's bust by C. Yamahata](https://sketchfab.com/3d-models/nefertitis-bust-like-in-the-museum-ce5b14926e494558ab584375a8d63ca7)
--- lib/tiny_obj_loader.h
+++ lib/tiny_obj_loader.h
@@ -650,7 +650,7 @@ bool ParseTextureNameAndOption(std::string *texname, texture_option_t *texopt,
 
 #endif  // TINY_OBJ_LOADER_H_
 
-#ifdef TINYOBJLOADER_IMPLEMENTATION
+//#ifdef TINYOBJLOADER_IMPLEMENTATION
 #include <cassert>
 #include <cctype>
 #include <cmath>
@@ -667,21 +667,21 @@ bool ParseTextureNameAndOption(std::string *texname, texture_option_t *texopt,
 
 #ifdef TINYOBJLOADER_DONOT_INCLUDE_MAPBOX_EARCUT
 // Assume earcut.hpp is included outside of tiny_obj_loader.h
-#else
+#	else
 
-#ifdef __clang__
-#pragma clang diagnostic push
-#pragma clang diagnostic ignored "-Weverything"
-#endif
+#		ifdef __clang__
+#			pragma clang diagnostic push
+#			pragma clang diagnostic ignored "-Weverything"
+#		endif
 
-#include <array>
-#include "mapbox/earcut.hpp"
+#		include <array>
+#		include "mapbox/earcut.hpp"
 
-#ifdef __clang__
-#pragma clang diagnostic pop
-#endif
+#		ifdef __clang__
+#			pragma clang diagnostic pop
+#		endif
 
-#endif
+#	endif
 
 #endif  // TINYOBJLOADER_USE_MAPBOX_EARCUT
 
@@ -3446,5 +3446,5 @@ bool ObjReader::ParseFromString(const std::string &obj_text,
 #pragma clang diagnostic pop
 #endif
 }  // namespace tinyobj
-
+#ifdef LOL
 #endif
